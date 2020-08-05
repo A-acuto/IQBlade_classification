e13416681e232b473c72967bb6c0d7c5268ffa71
Alberto Acuto <37350705+A-acuto@users.noreply.github.com>
2020-08-05 09:02:33 +0100

    Create webscraper.py

---
+++ python3.6/webscraper.py
@@ -0,0 +1,31 @@
+
+# 3.6 version compatible need to add a couple of details here
+
+def webscrape(companynumber, pages=10):
+    allText = ""
+​
+    try:
+        response = requests.get(f'http://crawler.iqblade.com:6969/api/scrapesite/{companynumber}/{pages}')
+        responseText = response.text
+        responseDict = json.loads(responseText)['scrapeData']
+​
+        if 'metaData' in responseDict:
+            meta = responseDict['metaData']
+            allText = meta['discription']
+​
+            keywords = meta['keyWords']
+​
+            for k in keywords:
+                allText = allText + f" {k}"
+​
+        if 'siteTesxt' in responseDict:
+            siteText = responseDict['siteTesxt']
+            for key in siteText:
+                textBlock = siteText[key]
+​
+                if textBlock not in allText:
+                    allText = allText + f" {textBlock}"
+    except:
+        print(f"Failed to retrieve site for {companynumber}")
+​
+    return allText
